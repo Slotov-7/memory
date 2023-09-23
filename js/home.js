6905bc8442572643b831690f5d383549415966db
redbdb <gustavoasamaral@gmail.com>
2023-09-23 19:19:26 -0300

    tela inicial melhorzinha

--- js/home.js
+++ js/home.js
@@ -7,4 +7,8 @@ document.getElementById('btn1').onclick = function() {
 document.getElementById('btn2').onclick = function() {
   window.location = 'game.html';
 }
+/*document.getElementById('btn3').onclick = function(){
+  window.location = '*seleção de tema*.html';
+}
+*/
 
