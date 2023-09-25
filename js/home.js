3b3e734786b82a8f024f5327c610a0d558bc6c9d
redbdb <gustavoasamaral@gmail.com>
2023-09-24 23:03:28 -0300

    play confirm done

--- js/home.js
+++ js/home.js
@@ -2,23 +2,29 @@
 const form = document.getElementById('myform');
 
 document.getElementById('btn1').onclick = function() {
-  //remove hidden from 'P1 insert name' prompt and text
-  document.getElementById('P1 txt').removeAttribute('hidden')
-  document.getElementById('P1 name').removeAttribute('hidden')
-  const P1 = document.getElementById('P1 name')
-  //redirect to game page
-  window.location = "../memory/pages/game.html";
+  //remove hidden from 'P1 insert name' prompt and text, also gives hidden to 'P2 insert name' prompt and text in case
+  //multiplayer option has been choosed before.
+  document.getElementById('P1_txt').hidden = false 
+  document.getElementById('P1_name').hidden = false
+  document.getElementById('P2_txt').hidden = true
+  document.getElementById('P2_name').hidden = true
+  document.getElementById('play').hidden = false
 }
 document.getElementById('btn2').onclick = function() {
   //remove hidden from 'P1 and P2 insert name' prompt and text
-  document.getElementById('P1 txt').removeAttribute('hidden')
-  document.getElementById('P1 name').removeAttribute('hidden')
-  document.getElementById('P2 txt').removeAttribute('hidden')
-  document.getElementById('P2 name').removeAttribute('hidden')
-  //redirect to game page
-  window.location = "../memory/pages/game.html";
+  document.getElementById('P1_txt').hidden = false
+  document.getElementById('P1_name').hidden = false
+  document.getElementById('P2_txt').hidden = false
+  document.getElementById('P2_name').hidden = false
+  document.getElementById('play').hidden = false
 }
 document.getElementById('btn3').onclick = function() {
+  //redirect to theme selection page
   window.location = "../memory/pages/theme.html";
 }
+document.getElementById('play').onclick = function(){
+//redirect to game page
+window.location = "../memory/pages/game.html";
+}
+
 
