eb3289dd3ccb9d9892115a6ab451abdbde52d3ed
Rafael <04rfl.1910@gmail.com>
2023-09-30 12:08:29 -0300

    fix theme;

--- js/home.js
+++ js/home.js
@@ -1,16 +1,16 @@
 let isMultiplayer = false;
 const form = document.getElementById('myform');
 
-document.getElementById('btn1').onclick = function() {
+document.getElementById('btn1').onclick = function () {
   //remove hidden do prompt P1 , também oculta o prompt P2 caso ele o botão multiplayer tenha sido pressionado antes
-  document.getElementById('P1_txt').hidden = false 
+  document.getElementById('P1_txt').hidden = false
   document.getElementById('P1_name').hidden = false
   document.getElementById('P2_txt').hidden = true
   document.getElementById('P2_name').hidden = true
   document.getElementById('play').hidden = false
   isMultiplayer = false;
 }
-document.getElementById('btn2').onclick = function() {
+document.getElementById('btn2').onclick = function () {
   //remove hidden do prompt P1 e P2
   document.getElementById('P1_txt').hidden = false
   document.getElementById('P1_name').hidden = false
@@ -19,11 +19,11 @@ document.getElementById('btn2').onclick = function() {
   document.getElementById('play').hidden = false
   isMultiplayer = true;
 }
-document.getElementById('btn3').onclick = function() {
+document.getElementById('btn3').onclick = function () {
   //redireciona para a pagina de seleção de tema
   window.location = "/pages/theme.html";
 }
-document.getElementById('play').onclick = function(){
+document.getElementById('play').onclick = function () {
   //armazena o nome do P1 e P2
   localStorage['player1Name'] = document.getElementById('P1_name').value;
   localStorage['player2Name'] = document.getElementById('P2_name').value;
--- js/theme.js
+++ js/theme.js
@@ -2,13 +2,13 @@
 document.documentElement.setAttribute('data-theme', localStorage.getItem('theme') || 'animal');
 
 
-document.getElementById('animal-theme')?.addEventListener('click', function() {
+document.getElementById('animal-theme')?.addEventListener('click', function () {
     document.documentElement.setAttribute('data-theme', 'animal');
     localStorage.setItem('theme', 'animal');
     alert("Tema alterado com sucesso!")
 });
 
-document.getElementById('ufs-theme')?.addEventListener('click', function() {
+document.getElementById('ufs-theme')?.addEventListener('click', function () {
     document.documentElement.setAttribute('data-theme', 'ufs');
     localStorage.setItem('theme', 'ufs');
     alert("Tema alterado com sucesso!")
