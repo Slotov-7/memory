3cd3388fec0f7c905695096b0c28a2e89d6dd22a
Slotov-7 <guilhermguilhermegui888@gmail.com>
2023-10-05 09:44:47 -0300

    err

--- js/game.js
+++ js/game.js
@@ -104,15 +104,8 @@ const changeTurn = () => {
   spanTurn.innerText = `Turn: ${turn ? localStorage['player2Name'] : localStorage['player1Name']}`;
 }
 
-<<<<<<< HEAD
-// Função que vê se as cartas são iguais ou não, 
-//se as cartas forem iguais ela disabilita as duas cartas
-// se não ela vai remover as duas cartas pois elas não são iguais
-// usando tambem a fução checkEndGame para ver se o jogo acabou.
-=======
 // Função para verificar se duas cartas são iguais.
 // Se forem iguais, as cartas são desativadas. Se não forem iguais, as cartas são viradas novamente.
->>>>>>> 7125a1a61240ba6b2b62f660810ed4ecb7fe06f6
 const checkCards = (firstCard, secondCard) => {
   let player = turn ? 1 : 0;
   if (isMultiplayer) {
